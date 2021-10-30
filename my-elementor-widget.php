a06253fb14fb458c2737bdde0d83a2fad15044b7
Tahir321 <tariqaptech9@gmail.com>
2021-10-30 18:00:43 +0500

    init

---
+++ my-elementor-widget.php
@@ -0,0 +1,139 @@
+<?php
+
+/**
+
+ * Plugin Name: My Elementor Widget
+
+ * Plugin URI: https://example.com
+
+ * Description: A custom elementor widget
+
+ * Plugin Name: My Elementor Widget
+ 
+ * Version: 1.0.0
+
+ * Author: Tahir Ali
+
+ * Author URI: https://tahir.me
+
+ * Text Domain: my-elementor-widgets
+ 
+ */
+
+if( !defined('ABSPATH')) exit();
+
+/**
+ * Elementor Extension mian class 
+ * @since 1.0.0
+*/
+
+class MY_ELementor_Widget{
+    
+    // Plugin version 
+    const VERSION = '1.0.0';
+
+    // Minimum PHP Version
+    const MINIMUM_ELEMENTOR_VERSION = '2.0.0';
+
+    // MINIMUM PHP VERSION
+    const MINIMUM_PHP_VERSION = '7.0';
+
+    // Instance
+    private static $_instance = null;
+
+    /**
+     * SIngLetone Instance Method
+     * @since 1.0.0
+     */
+
+     public static function instance(){
+         if(is_null( self::$_instance )){
+             self::$_instance = new self();
+         }
+         return self::$_instance;
+     }
+
+     /**
+      * Construct Method
+      * @since 1.0.0
+      */
+
+      public function __construct(){
+            // Call constants Method
+            $this->define_constants();
+            add_action('wp_equeue_script', [$this, 'scripts_style']);
+            add_action('init',[ $this, 'i18n' ]);
+      }
+
+      /**
+      * Define Plugin Constants
+      * @since 1.0.0
+      */
+
+      public function define_constants(){
+          define('MYEW_PLUGIN_URL',trailingslashit(plugins_url('/', __FILE__ )));
+          define('MYEW_PLUGIN_PATH',trailingslashit(plugin_dir_path( __FILE__ )));
+      }
+
+      /**
+       * Load Script & Style
+       * @since 1.0.0
+       */
+
+       public function script_style(){
+           wp_register_style('myew-style', MYEW_PLUGIN_URL . 'assets/dist/css/public.min.css', [], rand(), 'all');
+           wp_register_script('myew-script', MYEW_PLUGIN_URL . 'assets/dist/css/public.min.js', ['jquery'], rand(), true);
+
+           wp_enqueue_style('myew-style');
+           wp_enqueue_script('myew-script');
+       }
+
+       /**
+        * Load Text Domain dsfgdfgd
+        * @since 1.0.0
+        */
+
+        public function i18n(){
+            load_plugin_textdomain('my-elemntor',false, dirname( plugin_basename(__FILE__)) . '/languages');
+        }
+
+        /**
+         * Initialize the plugin
+         *  @since 1.0.0
+         */
+
+        public function init(){
+            add_action('elementor/init',[ $this, 'init_category' ]); 
+            add_action('elementor/widgets/wedgets_registered',[ $this, 'init_widgets' ]); 
+            
+        }
+
+        /**
+         * Init Widgets
+         *  @since 1.0.0
+         */
+
+         public function init_widgets(){
+            
+             require_once MYEW_PLUGINS_PATH . '/widgets/example.php';
+         }
+
+         /**
+          * Init Category Section
+          * @since 1.0.0
+          */
+
+          public function init_category(){
+              Elementor\Plugin::instance()->elements_manager->add_category(
+                  'myew-for-elementor',
+                  [
+                      'title' => 'My Elementor Widgets'
+                  ],
+                  1
+               );
+         }
+}
+
+MY_ELementor_Widget::instance();
+
+?>
---
+++ widgets/example.php
@@ -0,0 +1,65 @@
+<?php 
+
+namespace Elementor;
+
+class MYEW_Example_Wiget extends Widget_Base{
+    public function get_name(){
+        return 'myew-example-widget-id';
+    }
+
+    public function get_title(){
+        return esc_html__( 'Example Widgets', 'my-elementor-widgets' );
+    }
+
+    public function get_script_depends(){
+        return[
+            'myew-script'
+        ];
+    }
+
+    public function get_icon(){
+        return '';
+    }
+
+    public function get_categories(){
+        return [ 'myew-for-elementor' ];
+    }
+
+    public function _register_controls(){
+        // Content Settings
+        $this->start_controls_section(
+            'content_section',
+            [
+                'label' => __( 'Content Settings', 'my-elementor-widgets'),
+                'tab'   => \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
+        $this->end_controls_section(); 
+
+        // Styles Tab 
+        $this->style_tab();
+    }
+
+    private function style_tab(){
+
+    }
+
+    protected function render(){
+        $mywe_values = $this->get_setting_for_display();
+        ?>
+        
+        <div>
+            <h1>This is an examle Widget</h2>
+        </div>
+        
+        <?php
+    }
+
+    protected function content_template(){
+
+    }
+}
+
+Plugin::instance()->widgets_manager->register_widget_type(new MYEW_Example_Wiget());
+
+?>
